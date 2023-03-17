ebddd2063e031c0cd1d0a6212c681f6147d28a67
YooHyeok <yjk7454@naver.com>
2023-03-17 11:35:32 +0900

    Member Repository 생성

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    /**
+     * Spring boot가 @PersistenceContext 애노테이션을 확인한 뒤 EntityManager 빈을 주입한다.
+     */
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
