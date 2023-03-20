bb31d2dc7f07f0f9aa205afc16206d9b89005e1b
YooHyeok <yjk7454@naver.com>
2023-03-21 02:26:02 +0900

    Embaddable 타입에서 setter를 생성자로 대치 변경 불가능한 클래스

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,4 +10,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {// jpa가 엔티티를 생성할 때 Reflection이나 Proxy기능을 사용할때가 많다. (기본생성자를 통해 생성)
+    }
+
+    public Address(String city, String street, String zipcode) { // setter를 닫아두고 객체를 생성할 때만 값을 생성하도록
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
