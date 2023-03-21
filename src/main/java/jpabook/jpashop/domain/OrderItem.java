3b294cb82138fb5dc29bcbe6e43919c9ffb3b366
YooHyeok <yjk7454@naver.com>
2023-03-22 02:22:12 +0900

    상품 주문(OrderItem) 생성 메서드 추가

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -25,11 +25,26 @@ public class OrderItem {
 
     private int count;
 
+    // ==생성 메서드== //
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item); // 상품 추가
+        orderItem.setOrderPrice(orderPrice); // 주문 가격 추가
+        orderItem.setCount(count); // 주문 개수 추가
+
+        item.removeStock(count); // 현재 재고 감소
+        return orderItem;
+    }
+
     // ==비즈니스 로직== //
     public void cancel() {
         getItem().addStock(count); //Item의 재고 수량을 원상복구 시킨다.
     }
 
+    // ==조회 로직== //
+    /**
+     * 주문 상품 전체 가격 조회
+     */
     public int getTotalPrice() {
         return getOrderPrice() * getCount(); // 주문가격 * 개수
     }
