1ca38b6215127d282c858f6046a603d3b79572e3
YooHyeok <yjk7454@naver.com>
2023-03-22 00:29:50 +0900

    ItemService 추가 및 구현

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+}
