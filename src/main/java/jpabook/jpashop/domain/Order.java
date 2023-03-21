bf929d0fe2bb158d0a621325a20545fb22855125
YooHyeok <yjk7454@naver.com>
2023-03-21 11:50:38 +0900

    연관관계 편의 메소드 주석

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -34,9 +34,22 @@ public class Order {
     private OrderStatus status; //주문 상태 [ORDER, CANCEL] 주문/취소
 
     //==연관관계 메서드==//
+
+    /**
+     * 양방향 연관관계 설정시 Order와 Member에서 Member가 주문하면 orders 리스트에 해당 주문을 넣어줘야한다.
+     * 그래야 member.getOrders()로 주문한것을 찾을 수 있고, order.getMember()로 양방향이라는 객체적으로 공유할 수 있게 된다.
+     *
+     * 회원테이블의 주문리스트에 주문을 넣고 주문테이블에 회원필드에 해당 회원정보를 넣는다.
+     *
+     * Member member = new Member();
+     * Order order = new Order();
+     * member.getOrders().add(order); - member(회원) 객체의 orders 리스트에 order(주문) 추가
+     * order.setMember(member); - order(주문) 객체에 member(회원)을 추가
+     * @param member
+     */
     public void setMember(Member member) {
         this.member = member;
-        member.getOrders().add(this); // orders List에 member를 추가한다.
+        member.getOrders().add(this); // (양방향) orders List에 member를 추가한다.
     }
     public void addOrderItem(OrderItem orderItem) {
         orderItems.add(orderItem);
