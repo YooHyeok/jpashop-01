f23a8bc9e3537c320f0308b0525e05f0804069ab
YooHyeok <yjk7454@naver.com>
2023-03-22 20:55:58 +0900

    OrderRepository JPQL 동적쿼리 - findAllByString

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -3,8 +3,10 @@ package jpabook.jpashop.repository;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
@@ -26,5 +28,41 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {}
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        // JPA - JPQL 동적 쿼리
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false; // where문 첫 주입 후 두번째 조건이 존재한다면 and조건이 들어가야하므로 false로 초기화
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false; // where문 주입 후 컨디션 false
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000);//최대 1000건 조회
+
+        if (orderSearch.getOrderStatus() != null)
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        if (StringUtils.hasText(orderSearch.getMemberName()))
+            query = query.setParameter("name", orderSearch.getMemberName()); //Order와 연관된 member를 조인하는 join쿼리. o.member m 으로 표현한다.
+
+        return query.getResultList();
+    }
 }
