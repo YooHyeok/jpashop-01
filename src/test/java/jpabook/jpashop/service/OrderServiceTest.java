8a5b6cd5b095f7a60b7aa5e75f3dfab78b5114f3
YooHyeok <yjk7454@naver.com>
2023-03-22 19:57:59 +0900

    OrderServiceTest - 상품주문_재고수량초과 테스트

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -8,9 +8,7 @@ import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.exception.NotEnoughStockException;
 import jpabook.jpashop.repository.OrderRepository;
-import org.junit.Assert;
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -36,7 +34,7 @@ public class OrderServiceTest {
 
         //given
         Member member = createMember(); //회원 생성
-        Book book = createBook(); //책 생성
+        Book book = createBook("시골 JPA", 10000, 10); //책 생성
 
         //when
         int orderCount = 2;
@@ -52,9 +50,15 @@ public class OrderServiceTest {
     @Test(expected = NotEnoughStockException.class)
     public void 상품주문_재고수량초과() throws Exception {
         //given
+        Member member = createMember();
+        Item item = createBook("시골 JPA", 10000, 10);
+        int orderCount = 11;
+
         //when
+        orderService.order(member.getId(), item.getId(), orderCount);
 
         //then
+        fail("재고 수량 부족 예외가 발생해야 한다.");
 
     }
     @Test
@@ -67,21 +71,11 @@ public class OrderServiceTest {
 
     }
 
-    @Test
-    public void 상품주문_재고수량초과() throws Exception {
-        //given
-
-        //when
-
-        //then
-
-    }
-
-    private Book createBook() {
+    private Book createBook(String name, int price, int stockQuantity) {
         Book book = new Book();
-        book.setName("시골 JPA");
-        book.setPrice(10000);
-        book.setStockQuantity(10);
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
         em.persist(book);
         return book;
     }
