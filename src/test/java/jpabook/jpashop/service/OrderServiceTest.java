486a79ad1e5e362f4bd99e6102340fdcc4005998
YooHyeok <yjk7454@naver.com>
2023-03-22 19:50:56 +0900

    member, book 객체생성 및 영속화 메소드추출

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -6,6 +6,7 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import jpabook.jpashop.repository.OrderRepository;
 import org.junit.Assert;
 import org.junit.Test;
@@ -34,16 +35,8 @@ public class OrderServiceTest {
     public void 상품주문() throws Exception {
 
         //given
-        Member member = new Member(); //회원 생성
-        member.setName("회원1");
-        member.setAddress(new Address("서울", "강가", "123-123"));
-        em.persist(member);
-
-        Book book = new Book(); //책 생성
-        book.setName("시골 JPA");
-        book.setPrice(10000);
-        book.setStockQuantity(10);
-        em.persist(book);
+        Member member = createMember(); //회원 생성
+        Book book = createBook(); //책 생성
 
         //when
         int orderCount = 2;
@@ -56,7 +49,14 @@ public class OrderServiceTest {
         assertEquals("주문 가격은 가격 * 수량 이다.", 10000 * orderCount, getOrder.getTotalPrice()); //주문 수량은 2 이고 getTotalPrice는 10000을 두번 누적 합한다.
         assertEquals("주문 수량만큼 재고가 줄어야 한다.", 8, book.getStockQuantity()); //시골 JPA의 stockQuantity는 10 이고 주문수량은 2 이다.
     }
+    @Test(expected = NotEnoughStockException.class)
+    public void 상품주문_재고수량초과() throws Exception {
+        //given
+        //when
 
+        //then
+
+    }
     @Test
     public void 주문취소() throws Exception {
         //given
@@ -75,5 +75,22 @@ public class OrderServiceTest {
 
         //then
 
+    }
+
+    private Book createBook() {
+        Book book = new Book();
+        book.setName("시골 JPA");
+        book.setPrice(10000);
+        book.setStockQuantity(10);
+        em.persist(book);
+        return book;
+    }
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "강가", "123-123"));
+        em.persist(member);
+        return member;
     }
 }
