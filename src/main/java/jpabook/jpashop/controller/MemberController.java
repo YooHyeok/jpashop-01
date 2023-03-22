a93f67b3b8d0209afd379ad1803179ba691f7efc
YooHyeok <yjk7454@naver.com>
2023-03-23 01:19:20 +0900

    ddl auto update전략 변경/ MemberController 회원 목록 기능 추가/ memberList.html 추가

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -42,5 +43,20 @@ public class MemberController {
         memberService.join(member);
         
         return "redirect:/"; // 홈화면으로 redirect 새로고침
+    }
+
+    @GetMapping("members") // 맨앞에 / 생략가능
+    public String list(Model model) {
+        List<Member> members = memberService.findMember();
+        model.addAttribute("members", members);
+        /**
+         * api만들때는 절대 Entity를 외부로 반환하면 안된다.
+         * api라는것은 스펙이다. Member Entity를 반환하게되면
+         * 만약 Member에 Field를 추가하게되면 보안에도 문제가 발생할 뿐더러 API의 스펙이 변해버린다.
+         * 예를들어 userPassword라는 필드를 추가하게되면 패스워드가 그대로 노출된다.
+         * ㄴ 템플릿에서는 객체로 추출하여 접근하지만 react 혹은 javascript로 값을 파싱할때는 직접 접근해서 값을 확인하기 때문인거 같다.
+         * 엔티티에 로직을 추가하면 api의 스펙이 변경되고 이는 불안정한 API 스펙이 된다.
+         */
+        return "/members/memberList";
     }
 }
