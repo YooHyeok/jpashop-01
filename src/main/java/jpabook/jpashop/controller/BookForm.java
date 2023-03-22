f54b3b34e545fd0a698a3fc92daa438f9277dd85
YooHyeok <yjk7454@naver.com>
2023-03-23 01:36:01 +0900

    상품 등록 - ItemController, createItemForm.html 생성

---
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookForm {
+    private Long id;
+
+    /* 상품의 공통 속성 */
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    /* 책과 관련된 특별한 속성 */
+    private String author;
+    private String isbn;
+}
