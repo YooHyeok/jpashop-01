c5937b3eb6e3df42b149150eb6abfae7fd77c7bb
YooHyeok <yjk7454@naver.com>
2023-03-23 01:40:20 +0900

    상품 수정 - itemlist.html 및 ItemController 메소드 추가

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,17 +9,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemService itemService;
 
+    /**
+     * 상품 등록 폼
+     */
     @GetMapping("items/new")
     public String createForm(Model model) {
         model.addAttribute("form", new BookForm());
         return "items/createItemForm";
     }
 
+    /**
+     * 상품 등록
+     */
     @PostMapping("/items/new")
     public String create(BookForm form) {
         Book book = new Book();
@@ -29,5 +38,15 @@ public class ItemController {
         book.setIsbn(form.getIsbn());
         itemService.saveItem(book);
         return "redirect:/"; //저장된 책 목록으로 이동계획.
+    }
+
+    /**
+     * 상품 목록 조회
+     */
+    @GetMapping("items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemlist";
     }
 }
