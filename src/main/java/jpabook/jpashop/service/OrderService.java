a8093bc36f361cb5fdb0588485549b077d10450b
YooHyeok <yjk7454@naver.com>
2023-03-22 18:34:02 +0900

    주문 서비스 - 주문 기능

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,56 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    //주문
+
+    /**
+     * 1. [member, item] 엔티티 조회(회원, 상품)
+     * 2. [delevery] 배송정보 생성(member.주소)
+     * 3. [orderItem] 주문상품 생성(item, item.가격, 갯수)
+     * 4. [order] 주문 생성(member, delevery, orderItem)
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+        // 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // 배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        // 주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem); //cascadeType.All 옵션에 의해 orderItems와 delivery가 자동으로 persist 저장된다.
+        // Cascade범위 : order같은 경우 order가 delivery와 orderItems를 관리할때 즉, 참조하는 주인이 private oner인경우(다른곳에서 참조하지않는) - delevery와 orderItems는 order만 참조해서 쓴다.
+
+        //주문 저장
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    //취소
+
+    //검색
+}
