805827fbf67937a4a7011f4d8e7d8185e93c9fe2
YooHyeok <yjk7454@naver.com>
2023-03-23 12:26:26 +0900

    주문 목록 검색, 주문취소 기능추가 - OrderConroller.orderList() & OrderController.cancelOrder() & orderList.html

--- src/main/java/jpabook/jpashop/controller/OrderConroller.java
+++ src/main/java/jpabook/jpashop/controller/OrderConroller.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
 import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.MemberService;
 import jpabook.jpashop.service.OrderService;
@@ -9,6 +11,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -29,5 +34,27 @@ public class OrderConroller {
         model.addAttribute("members", members);
         model.addAttribute("items", items);
         return "order/orderForm";
+    }
+
+    @PostMapping("order")
+    public String order(Long memberId, Long itemId, int count) {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+    /**
+     * 주문 목록
+     */
+    @GetMapping("orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) { //@ModelAttribute는 기본적으로 Model의 기능을 함께 한다. 생략도 가능하다.
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+        return "/order/orderList";
+    }
+
+    @PostMapping("orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
     }
 }
